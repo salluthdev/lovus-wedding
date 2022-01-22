cc5dea5c76f2c6cd14ba4f875f5a3cfcc34803b5
Faisal L <salluthdev@gmail.com>
2022-01-22 11:52:32 +0700

    Penambahan Script JS - Tab Link Styling

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,23 @@ var swiper = new Swiper(".mySwiper", {
 	  prevEl: '.swiper-button-prev',
 	},
 });
+
+function openPage(pageName,elmnt,color)
+{
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "";
+  }
+  document.getElementById(pageName).style.display = "flex";
+  elmnt.style.backgroundColor = color;
+  document.getElementById(pageName).style.display = "flex";
+  elmnt.style.color = "#fff";
+}
+
+document.getElementById("defaultOpen").click();
